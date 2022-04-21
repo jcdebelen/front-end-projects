438645eadeb36cc29e33e2559fb26192426ee4e5
JC De Belen <debelenjc@gmail.com>
2022-04-21 13:08:11 +0800

    added animations

--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -1,224 +1,248 @@
-let board = [['','',''],['','',''],['','','']];
-let moves = [];
-let player;
 const dom = document;
-let win=0;
 
-dom.getElementById('O').addEventListener('click', function() {
-    player = 'O';
-    select();
-    oSpanColor();
-})
+let board;
+let moves;
+let player;
+let win;
+let z;
+const cells = dom.getElementsByClassName("cell");
 
-dom.getElementById('X').addEventListener('click', function() {
-    player = 'X';
-    select();
-    xSpanColor();
-})
+function reset() {
+  board = [
+    ["", "", ""],
+    ["", "", ""],
+    ["", "", ""],
+  ];
+  moves = [];
+  win = 0;
+  dom.getElementById("controls").style.visibility = "hidden";
+  dom.getElementById("controls").style.animation = "none";
+  updateCells();
+  dom.getElementById("player").style.display = "block";
+  dom.getElementById("congrats").style.display = "none";
+}
+reset();
+
+//Select player
+dom.getElementById("O").addEventListener("click", function () {
+  player = "O";
+  select();
+  oSpanColor();
+});
+
+dom.getElementById("X").addEventListener("click", function () {
+  player = "X";
+  select();
+  xSpanColor();
+});
 
 function select() {
-    dom.getElementById('start').style.display = 'none';
-    dom.getElementById('maingame').style.display = 'flex'
-    dom.getElementById('turn').innerHTML = ' ' + player + ' ';
+  dom.getElementById("start").style.display = "none";
+  dom.getElementById("maingame").style.display = "flex";
+  dom.getElementById("cover").style.animation = "spread 0.5s";
+  dom.getElementById("cover").style.height = "60vmin";
+  dom.getElementById("cover").style.width = "60vmin";
+  setTimeout(displayTurn, 300);
 }
 
-
-const cells = dom.getElementsByClassName('cell');
-let x;
-for (let i=0;i<cells.length;i++) {
-    cells[i].addEventListener('click', function() {
-        if (win === 1) {
-        }
-        else if (cells[i].innerHTML === "") {
-            x=i;
-            x=Math.floor(x/3);
-            playerColor(i);
-            if (i>2) {
-                i=i%3;
-            }
-            board[x][i] = player;
-            updateCells();
-            changeplayer();
-            moves.push(structuredClone(board));
-            dom.getElementById('turn').innerHTML = ' ' + player + ' ';
-            checkWin();  
-            i=null;x=null;;
-        }
-    })
+function displayTurn() {
+  dom.getElementById("player").style.visibility = "visible";
+  dom.getElementById("player").style.animation = "fadeIn .5s";
+  dom.getElementById("turn").innerHTML = " " + player + " ";
+  dom.getElementById("score").style.visibility = "visible";
 }
 
-let z;
+//Cell Event Listener
+for (let i = 0; i < cells.length; i++) {
+  cells[i].addEventListener("click", function () {
+    if (win === 1) {
+    } else if (cells[i].innerHTML === "") {
+      playerColor(i);
+      board[Math.floor(i / 3)][i % 3] = player;
+      updateCells();
+      changeplayer();
+      moves.push(structuredClone(board));
+      dom.getElementById("turn").innerHTML = " " + player + " ";
+      checkWinV2();
+    }
+  });
+}
+
+//Display to HTML
 function updateCells() {
-    for (let a=0;a<3;a++) {
-        for (let b=0;b<3;b++) {
-            z=(a*3)+b;
-            cells[z].innerHTML = '';
-            if (board[a][b] === 'X') {
-                cells[z].innerHTML = 'X';
-            }
-            else if (board[a][b] === 'O') {
-                cells[z].innerHTML = 'O';
-            }
-        }
+  for (let a = 0; a < 3; a++) {
+    for (let b = 0; b < 3; b++) {
+      z = a * 3 + b;
+      cells[z].innerHTML = "";
+      if (board[a][b] === "X") {
+        cells[z].innerHTML = "X";
+      } else if (board[a][b] === "O") {
+        cells[z].innerHTML = "O";
+      }
     }
+  }
 }
 
-function changeplayer () {
-    if (player === 'O') {
-        player = 'X';
-    }
-    else if (player === 'X') {
-        player = 'O';
-    }
+//Change Player
+function changeplayer() {
+  if (player === "O") {
+    player = "X";
+  } else if (player === "X") {
+    player = "O";
+  }
 }
 
+//Change Player Color
 function xSpanColor() {
-    dom.getElementById('turn').style.color = 'black';
-    dom.getElementById('turn').style.textShadow = '1px 1px white';
+  dom.getElementById("turn").style.color = "rgb(41, 41, 41)";
+  dom.getElementById("turn").style.textShadow = "1px 1px white";
 }
 
 function oSpanColor() {
-    dom.getElementById('turn').style.color = 'white';
-    dom.getElementById('turn').style.textShadow = '1px 1px black';
+  dom.getElementById("turn").style.color = "#e5efc1";
+  dom.getElementById("turn").style.textShadow = "1px 1px black";
 }
 
 function playerColor(i) {
-    if (player === 'O') {
-        cells[i].style.color = 'white';
-        cells[i].style.textShadow = '1px 1px black';
-        xSpanColor();
-    }
-    else if (player === 'X') {
-        cells[i].style.color = 'black';
-        cells[i].style.textShadow = '1px 1px white';
-        oSpanColor();   
-    }
+  if (player === "O") {
+    cells[i].style.color = "#e5efc1";
+    cells[i].style.textShadow = "1px 1px black";
+    xSpanColor();
+  } else if (player === "X") {
+    cells[i].style.color = "rgb(41, 41, 41)";
+    cells[i].style.textShadow = "1px 1px white";
+    oSpanColor();
+  }
 }
 
+let xscore = 0;
+let oscore = 0;
 let h;
-function playerW(a = 0) {
-    win=1;
-    if (a===0) {
-        dom.getElementById('player').innerHTML = 'Player O wins!';
-        showControls ()
+function playerW(a) {
+  win = 1;
+  if (a === "O") {
+    dom.getElementById("congrats").innerHTML = "Player O wins!";
+    dom.getElementById("player").style.display = "none";
+    dom.getElementById("congrats").style.display = "block";
+    oscore++;
+    dom.getElementById("oScore").innerHTML = oscore;
+  } else if (a === "X") {
+    dom.getElementById("congrats").innerHTML = "Player X wins!";
+    dom.getElementById("player").style.display = "none";
+    dom.getElementById("congrats").style.display = "block";
+    xscore++;
+    dom.getElementById("xScore").innerHTML = xscore;
+  } else if (a === 0) {
+    dom.getElementById("congrats").innerHTML = "DRAW!";
+    dom.getElementById("player").style.display = "none";
+    dom.getElementById("congrats").style.display = "block";
+  }
+  dom.getElementById("congrats").style.color = "white";
+  dom.getElementById("congrats").style.textShadow = "2px 2px black";
+  h = moves.length - 1;
+  disableButton();
+  dom.getElementById("congrats").style.animation = "breath 4s";
+  showControls();
+}
+
+function showControls() {
+  dom.getElementById("controls").style.visibility = "visible";
+  dom.getElementById("controls").style.animation = "fadeIn 3s";
+}
+
+//Check Win V2
+let check;
+function checkWinV2() {
+  //Horizontal Check
+  for (i = 0; i < 3; i++) {
+    if (board[i][0] !== "") {
+      check = board[i][0];
+      if (
+        board[i][0] === check &&
+        board[i][1] === check &&
+        board[i][2] === check
+      ) {
+        playerW(check);
+      }
     }
-    else if (a===1) {
-        dom.getElementById('player').innerHTML = 'Player X wins!';
-        showControls ()
+  }
+  //Vertical Check
+  for (i = 0; i < 3; i++) {
+    if (board[0][i] !== "") {
+      check = board[0][i];
+      if (
+        board[0][i] === check &&
+        board[1][i] === check &&
+        board[2][i] === check
+      ) {
+        playerW(check);
+      }
     }
-    else if (a===2) {
-        dom.getElementById('player').innerHTML = 'Tied Game!';
-        dom.getElementById('reset').style.visibility = 'visible';
-        dom.getElementById('reset').style.animation = 'fadeIn 2s';
+  }
+  //Diagonal Check
+  if (board[1][1] !== "") {
+    check = board[1][1];
+    if (
+      board[0][0] === check &&
+      board[1][1] === check &&
+      board[2][2] === check
+    ) {
+      playerW(check);
+    } else if (
+      board[0][2] === check &&
+      board[1][1] === check &&
+      board[2][0] === check
+    ) {
+      playerW(check);
     }
-    dom.getElementById('player').style.color = 'white';
-    dom.getElementById('player').style.textShadow = '2px 2px black';
-    h=moves.length-1;
-    dom.getElementById('movenum').innerHTML = 'Move number: ' + parseInt(h+1);
+  }
+  if (win === 0) {
+    if (moves.length === 9) {
+      playerW(0);
+    }
+  }
+}
+
+//Controls
+dom.getElementById("prev").addEventListener("click", function () {
+  if (h > 0) {
+    h--;
+    board = moves[h];
+    updateCells();
     disableButton();
-    dom.getElementById('player').style.animation = 'breath 4s';
-
-}
-
-function showControls () {
-    dom.getElementById('controls').style.visibility = 'visible';
-    dom.getElementById('controls').style.animation = 'fadeIn 3s';
-}
-
-// Check Win
-let o;
-function checkWin() {
-    if (board[0][0] === 'O' && board[0][1] ==='O' && board[0][2] === 'O') {
-        playerW(0);
-    }
-    else if (board[1][0] === 'O' && board[1][1] ==='O' && board[1][2] === 'O') {
-        playerW(0);
-    }
-    else if (board[2][0] === 'O' && board[2][1] ==='O' && board[2][2] === 'O') {
-        playerW(0);
-    }
-    else if (board[0][0] === 'O' && board[1][0] ==='O' && board[2][0] === 'O') {
-        playerW(0);
-    }
-    else if (board[0][1] === 'O' && board[1][1] ==='O' && board[2][1] === 'O') {
-        playerW(0);
-    }
-    else if (board[0][2] === 'O' && board[1][2] ==='O' && board[2][2] === 'O') {
-        playerW(0);
-    }
-    else if (board[0][0] === 'O' && board[1][1] ==='O' && board[2][2] === 'O') {
-        playerW(0);
-    }
-    else if (board[0][2] === 'O' && board[1][1] ==='O' && board[2][0] === 'O') {
-        playerW(0);
-    }
-    else if (board[0][0] === 'X' && board[0][1] ==='X' && board[0][2] === 'X') {
-        playerW(1);
-    }
-    else if (board[1][0] === 'X' && board[1][1] ==='X' && board[1][2]=== 'X') {
-        playerW(1);
-    }
-    else if (board[2][0] === 'X' && board[2][1] ==='X' && board[2][2]=== 'X') {
-        playerW(1);
-    }
-    else if (board[0][0] === 'X' && board[1][0] ==='X' && board[2][0] === 'X') {
-        playerW(1);
-    }
-    else if (board[0][1] === 'X' && board[1][1] ==='X' && board[2][1] === 'X') {
-        playerW(1);
-    }
-    else if (board[0][2] === 'X' && board[1][2] ==='X' && board[2][2] === 'X') {
-        playerW(1);
-    }
-    else if (board[0][0] === 'X' && board[1][1] ==='X' && board[2][2] === 'X') {
-        playerW(1);
-    }
-    else if (board[0][2] === 'X' && board[1][1] ==='X' && board[2][0] === 'X') {
-        playerW(1);
-    }
-    else if (moves.length == 9) {
-        playerW(2);
-    }
-}
-
-dom.getElementById('prev').addEventListener('click', function() {
-    if (h>0) {
-        h--;
-        board=moves[h];
-        updateCells();
-        disableButton();
-        dom.getElementById('movenum').innerHTML = 'Move number: ' + parseInt(h+1);
-    }
+  }
 });
 
-dom.getElementById('next').addEventListener('click', function() {
-    if (h<(moves.length-1)) {
-        h++;
-        board=moves[h];
-        updateCells();
-        disableButton();
-        dom.getElementById('movenum').innerHTML = 'Move number: ' + parseInt(h+1);
-    }
+dom.getElementById("next").addEventListener("click", function () {
+  if (h < moves.length - 1) {
+    h++;
+    board = moves[h];
+    updateCells();
+    disableButton();
+  }
 });
-
 
 function disableButton() {
-    if (h===0) {
-        dom.getElementById('prev').style.backgroundColor = "#e7e7e7";
-        document.getElementById("prev").disabled = true;
-    }
-    else if (h===moves.length-1) {
-        dom.getElementById('next').style.backgroundColor = "#e7e7e7";
-        document.getElementById("next").disabled = true;
-    }
-    else {
-        dom.getElementById('next').style.backgroundColor = "#4CAF50";
-        dom.getElementById('prev').style.backgroundColor = "#008CBA";
-        document.getElementById("prev").disabled = false;
-        document.getElementById("next").disabled = false;
-    }
+  if (h === 0) {
+    dom.getElementById("prev").style.backgroundColor = "#e7e7e7";
+    document.getElementById("prev").disabled = true;
+  } else if (h === moves.length - 1) {
+    dom.getElementById("next").style.backgroundColor = "#e7e7e7";
+    document.getElementById("next").disabled = true;
+  } else {
+    dom.getElementById("next").style.backgroundColor = "#6fc772";
+    dom.getElementById("prev").style.backgroundColor = "#008CBA";
+    document.getElementById("prev").disabled = false;
+    document.getElementById("next").disabled = false;
+  }
 }
 
-dom.getElementById('reset').addEventListener('click', function() {
-    location.reload();
+//New game
+dom.getElementById("newgame").addEventListener("click", function () {
+  reset();
+});
+
+//Reset game
+dom.getElementById("reset").addEventListener("click", function () {
+  location.reload();
 });
