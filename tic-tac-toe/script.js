eb28eb610852f85e7f1959fade59e9b0c8b55995
JC De Belen <debelenjc@gmail.com>
2022-04-23 12:30:13 +0800

    added line animations

--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -11,12 +11,12 @@ const cells = dom.getElementsByClassName("cell");
 
 function reset() {
   dom.getElementById("cover").style.animation = "none";
-  dom.getElementById("controls").style.animation = "none";
   dom.getElementById("controls").style.visibility = "hidden";
-  dom.getElementById("congrats").style.display = "none";
   dom.getElementById("player").style.display = "block";
   dom.getElementById("player").style.visibility = "hidden";
   dom.getElementById("player").style.animation = "none";
+  dom.getElementById("congrats").style.display = "none";
+  hideLines();
   board = [
     ["", "", ""],
     ["", "", ""],
@@ -25,7 +25,7 @@ function reset() {
   moves = [];
   win = 0;
   updateCells();
-  setTimeout(animateBoard, 300);
+  setTimeout(animateBoard, 50);
   if (player !== "") {
     setTimeout(displayTurn, 700);
   }
@@ -150,27 +150,28 @@ let oscore = 0;
 function playerW(a) {
   win = 1;
   if (a === "O") {
-    dom.getElementById("congrats").innerHTML = "O wins!";
-    dom.getElementById("player").style.display = "none";
-    dom.getElementById("congrats").style.display = "block";
+    dom.getElementById("player").style.visibility = "hidden";
     oscore++;
+    dom.getElementById("congrats").innerHTML =
+      '<span id="playercolor">O</span> wins!';
+    dom.getElementById("playercolor").style.textShadow = "3px 3px black";
     dom.getElementById("oScore").innerHTML = oscore;
+    setTimeout(sayCongrats, 500);
   } else if (a === "X") {
-    dom.getElementById("congrats").innerHTML = "✖ wins!";
-    dom.getElementById("player").style.display = "none";
-    dom.getElementById("congrats").style.display = "block";
+    dom.getElementById("player").style.visibility = "hidden";
+    dom.getElementById("congrats").innerHTML =
+      '<span id="playercolor">✖</span> wins!';
+    dom.getElementById("playercolor").style.color = "rgb(75, 75, 75)";
+    dom.getElementById("playercolor").style.textShadow = "none";
     xscore++;
     dom.getElementById("xScore").innerHTML = xscore;
+    setTimeout(sayCongrats, 500);
   } else if (a === 0) {
     dom.getElementById("congrats").innerHTML = "DRAW!";
-    dom.getElementById("player").style.display = "none";
-    dom.getElementById("congrats").style.display = "block";
+    sayCongrats();
   }
-  dom.getElementById("congrats").style.color = "white";
-  dom.getElementById("congrats").style.textShadow = "2px 2px black";
   h = moves.length - 1;
   disableButton();
-  dom.getElementById("congrats").style.animation = "spreadOutIn 1.5s";
   showControls();
 
   var elementsX = document.getElementsByClassName("shapeX");
@@ -183,6 +184,14 @@ function playerW(a) {
   for (i = 0; i < elementsO.length; i++) {
     elementsO[i].style.animation = "none";
   }
+}
+
+function sayCongrats() {
+  dom.getElementById("player").style.display = "none";
+  dom.getElementById("congrats").style.display = "block";
+  dom.getElementById("congrats").style.visibility = "hidden";
+  dom.getElementById("congrats").style.visibility = "visible";
+  dom.getElementById("congrats").style.animation = "spreadOutIn 1.5s";
 }
 
 function showControls() {
@@ -202,6 +211,8 @@ function checkWinV2() {
         board[i][1] === check &&
         board[i][2] === check
       ) {
+        dom.getElementById("h" + i.toString()).style.visibility = "visible";
+        dom.getElementById("h" + i.toString()).style.animation = "extend 0.7s";
         playerW(check);
       }
     }
@@ -215,6 +226,8 @@ function checkWinV2() {
         board[1][i] === check &&
         board[2][i] === check
       ) {
+        dom.getElementById("v" + i.toString()).style.visibility = "visible";
+        dom.getElementById("v" + i.toString()).style.animation = "extendV 0.7s";
         playerW(check);
       }
     }
@@ -227,12 +240,16 @@ function checkWinV2() {
       board[1][1] === check &&
       board[2][2] === check
     ) {
+      dom.getElementById("d1").style.visibility = "visible";
+      dom.getElementById("d1").style.animation = "diagEx 0.7s";
       playerW(check);
     } else if (
       board[0][2] === check &&
       board[1][1] === check &&
       board[2][0] === check
     ) {
+      dom.getElementById("d2").style.visibility = "visible";
+      dom.getElementById("d2").style.animation = "diag2Ex 0.7s";
       playerW(check);
     }
   }
@@ -250,6 +267,7 @@ dom.getElementById("prev").addEventListener("click", function () {
     board = moves[h];
     disableButton();
     updateCells();
+    hideLines();
   }
 });
 
@@ -281,7 +299,8 @@ function disableButton() {
 
 //New game
 dom.getElementById("newgame").addEventListener("click", function () {
-  reset();
+  dom.getElementById("controls").style.animation = "fadeOut 1s";
+  setTimeout(reset, 350);
 });
 
 //Reset game
@@ -295,4 +314,13 @@ function removeAnim(z) {
   anim[0].style.visibility = "hidden";
   anim[0].style.animation = "none";
   anim[0].style.visibility = "visible";
+}
+
+//hide winning lines
+function hideLines() {
+  const lines = dom.getElementsByTagName("hr");
+  for (let i = 0; i < lines.length; i++) {
+    lines[i].style.visibility = "hidden";
+    lines[i].style.animation = "none";
+  }
 }
